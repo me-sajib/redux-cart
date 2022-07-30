5765aae26e298828f3f6baf8525352bae52f07ec
sajib <me.mrsajib@gmail.com>
2022-07-30 13:39:52 +0600

    add state reducer

--- src/redux/actions/cartAction.js
+++ src/redux/actions/cartAction.js
@@ -1,16 +1,18 @@
 export const ADD_TO_CART = "ADD_CART";
 export const REMOVE_TO_CART = "REMOVE_CART";
 
-export const addToCart = (id) => {
+export const addToCart = (id, name) => {
   return {
     type: ADD_TO_CART,
     id: id,
+    name,
   };
 };
 
-export const removeToCart = (id) => {
+export const removeToCart = (id, name) => {
   return {
     type: REMOVE_TO_CART,
     id: id,
+    name,
   };
 };
---
+++ src/redux/reducers/cartReducer.js
@@ -0,0 +1,31 @@
+import { ADD_TO_CART, REMOVE_TO_CART } from "../actions/cartAction";
+
+const initialState = {
+  cart: [],
+  products: [
+    { id: 1, name: "Hino AC", price: 122 },
+    { id: 2, name: "Asus AC", price: 122 },
+    { id: 3, name: "Honda AC", price: 122 },
+    { id: 4, name: "Hino AC", price: 122 },
+    { id: 5, name: "Hino AC", price: 122 },
+  ],
+};
+const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const newItem = {
+        productId: action.id,
+        name: action.name,
+        cartId: state.cart.length + 1,
+      };
+      const newCart = [...state.cart, newItem];
+      return { ...state, cart: newCart };
+    case REMOVE_TO_CART:
+      const remainingCart = state.filter((i) => i.id !== action.id);
+      return { ...state, cart: remainingCart };
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
